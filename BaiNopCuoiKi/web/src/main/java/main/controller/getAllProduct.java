a1a9e125b3f1e5a2cf8726e1d8a357e7a64f545e
TranQuangVu249 <20130470@st.hcmuaf.edu.vn>
2023-01-03 21:21:13 +0700

    Update getAllProduct.java

--- BaiNopCuoiKi/web/src/main/java/main/controller/getAllProduct.java
+++ BaiNopCuoiKi/web/src/main/java/main/controller/getAllProduct.java
@@ -12,7 +12,18 @@ import java.util.List;
 
 @WebServlet(name = "getAllProduct", value = "/getAllProduct")
 public class getAllProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<User> user = useService.getInstance().getAllUser();
+        request.setAttribute("allproduct", products);
+        request.getRequestDispatcher("shop_gird.jsp").forward(request, response);
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
 
 
 
