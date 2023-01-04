d47a220eff7ba396af45bc669cdc7035e5e94135
TranQuangVu249 <20130470@st.hcmuaf.edu.vn>
2023-01-04 13:35:45 +0700

    Revert "Merge pull request #4 from DaoLongVu20130466/QuangVu"
    This reverts commit 8fd7ab65384fc33a8ecbb3acaf9a694b328e8191, reversing
    changes made to 94fc253853e6b0d2ae27b5af327f981e4b034210.

--- BaiNopCuoiKi/web/src/main/java/main/controller/getAllProduct.java
+++ BaiNopCuoiKi/web/src/main/java/main/controller/getAllProduct.java
@@ -3,36 +3,40 @@ package main.controller;
 import main.bean.Products;
 import main.services.ProductsService;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "getAllProduct", value = "/getAllProduct")
 public class getAllProduct extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 <<<<<<< Updated upstream
-        ArrayList<User> user = useService.getInstance().getAllUser();
-        request.setAttribute("allproduct", products);
-        request.getRequestDispatcher("shop_grid.jsp").forward(request, response);
+        String productinfor = request.getParameter("productInfor");
+        List<Products> products = new ArrayList<>();
+        if (productinfor != null) {
+            products = ProductsService.getInstance().getAllproducts();
+        }
+        else {
+            products = ProductsService.getInstance().getProductByAttb(productinfor);
+
+        }
+        if(products!= null) {
+            request.setAttribute("allproduct", products);
+            request.getRequestDispatcher("shop.jsp").forward(request, response);
+        }
 =======
-        ArrayList<Products> products = (ArrayList<Products>) ProductsService.getInstance().getAllproducts();
-        request.setAttribute("allproducts", products);
-        request.getRequestDispatcher("shop-grid.jsp").forward(request, response);
+        ArrayList<User> user = useService.getInstance().getAllUser();
+        request.setAttribute("allproduct", Products);
+        request.getRequestDispatcher("shop_gird.jsp").forward(request, response);
 >>>>>>> Stashed changes
     }
-
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-}
-
-
-
 }
