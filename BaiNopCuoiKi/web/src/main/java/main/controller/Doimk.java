6d37afae398f96f9d5cd139555b61d758f01bdd9
TranQuangVu249 <20130470@st.hcmuaf.edu.vn>
2023-01-10 13:39:08 +0700

    up

--- BaiNopCuoiKi/web/src/main/java/main/controller/Doimk.java
+++ BaiNopCuoiKi/web/src/main/java/main/controller/Doimk.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class Doimk extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("DoiMatKhau.jsp").forward(request, response);
     }
 
     @Override
