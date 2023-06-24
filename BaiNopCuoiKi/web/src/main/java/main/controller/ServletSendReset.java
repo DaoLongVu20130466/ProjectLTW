cde2eb4984d0edb08b0c7c452c8b19f3cc540e28
DaoLongVu20130466 <20130466@st.hcmuaf.edu.vn>
2023-06-24 23:26:24 +0700

    check

--- BaiNopCuoiKi/web/src/main/java/main/controller/ServletSendReset.java
+++ BaiNopCuoiKi/web/src/main/java/main/controller/ServletSendReset.java
@@ -10,7 +10,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "ServletSendReset", value = "/ServletSendReset")
+    @WebServlet(name = "ServletSendReset", value = "/ServletSendReset")
 public class ServletSendReset extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
